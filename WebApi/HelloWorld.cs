5b066ca071ad8732494d3bf5c2fd7edbaaf3d07b
Idam Achmad Faizin <idamachmadfaizin@gmail.com>
2024-09-11 22:50:48 +0700

    feat: add FastEndpoints.

---
+++ WebApi/HelloWorld.cs
@@ -0,0 +1,15 @@
+namespace WebApi;
+
+sealed class HelloWorld : EndpointWithoutRequest
+{
+    public override void Configure()
+    {
+        Get("/api/HelloWorld");
+        AllowAnonymous();
+    }
+    
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        await SendOkAsync("Hello world", ct);
+    }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,6 +1,37 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using FastEndpoints.Security;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+builder.Services
+    .AddAuthenticationJwtBearer(o => o.SigningKey = builder.Configuration["Auth:SigningKey"])
+    .AddAuthorization()
+    .AddFastEndpoints()
+    .AddResponseCaching();
+
+if (!builder.Environment.IsProduction())
+{
+    builder.Services.SwaggerDocument(o =>
+    {
+        o.DocumentSettings = settings =>
+        {
+            settings.DocumentName = "v0";
+            settings.Version = "0.0.0";
+        };
+        o.FlattenSchema = true;
+    });
+}
+
+var app = builder.Build();
+app.UseAuthentication()
+    .UseAuthorization()
+    .UseDefaultExceptionHandler()
+    .UseFastEndpoints(config => config.Errors.UseProblemDetails());
+
+if (!app.Environment.IsProduction())
+{
+    app.UseSwaggerGen(uiConfig: settings => settings.DeActivateTryItOut());
+}
+
+app.MapGet("/", () => Results.Redirect("/swagger"))
+    .ExcludeFromDescription();
 
 app.Run();
